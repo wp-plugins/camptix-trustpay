e60705e40718a1f47ad5875d1a810e67ba5a5053
JohnnyPea <JohnnyPea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-02 23:19:17 +0000

    First release
    git-svn-id: https://plugins.svn.wordpress.org/camptix-trustpay/trunk@1103643 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wi-trustpay.php
@@ -0,0 +1,98 @@
+<?php
+
+if(!class_exists('WI_Trustpay'))
+{ 		
+	class WI_Trustpay
+	{
+		const INIT = 'INIT';
+		const VALID = 'VALID';
+		const NOT_VALID = 'NOT_VALID';
+		const INCOMPLETE = 'INCOMPLETE';
+		const SIGNED = 'SIGNED';
+		
+		const SIGN_NOT_SIGNED = 'SIGN_NOT_SIGNED';
+		const SIGN_NOT_VALID = 'SIGN_NOT_VALID';
+		
+		const RES_NOTSIGNED = 'RES_NOTSIGNED';
+		const RES_FAILED = 'RES_FAILED';
+		const RES_TO = 'RES_TO';
+		const RES_OK = 'RES_OK';
+
+		const SIGN_FIELD = 'SIG';
+		
+		protected $required_fields;
+	    protected $optional_fields;
+	    
+	    protected $store_key;
+	    protected $is_test;
+	    protected $fields;
+	    protected $status;
+	    protected $result;
+	    
+		function getResult()
+		{
+		    return $this->result;
+		}
+
+		function getStatus()
+		{
+		    return $this->status;
+		}
+		
+		public function __get($name)
+	    {
+	        if(isset($this->fields[$name]))
+	            return $this->fields[$name];
+
+	        return null;
+	    }
+
+	    public function __isset($name)
+	    {
+	        return isset($this->fields[$name]);
+	    }
+
+	    function toArray()
+	    {
+	        return $this->fields;
+	    }
+
+	    static function get_valid_currencies()
+	    {
+	    	return array(
+	    		'EUR' => 'EUR',
+                'CZK' => 'CZK',
+                'GBP' => 'GBP',
+                'HUF' => 'HUF',
+                'PLN' => 'PLN',
+                'USD' => 'USD',
+                'RON' => 'RON',
+                'BGN' => 'BGN',
+                'HRK' => 'HRK',
+                'LTL' => 'LTL',
+                'TRY' => 'TRY'
+	    	);
+	    }
+
+	    static function get_valid_languages()
+	    {
+			return array(
+				'bg' => 'bg', 
+				'bs' => 'bs', 
+				'cs' => 'cs', 
+				'en' => 'en', 
+				'et' => 'et', 
+				'hr' => 'hr', 
+				'hu' => 'hu', 
+				'lt' => 'lt', 
+				'lv' => 'lv', 
+				'pl' => 'pl', 
+				'ro' => 'ro', 
+				'ru' => 'ru', 
+				'sk' => 'sk', 
+				'sl' => 'sl', 
+				'sr' => 'sr'
+			);
+		}
+	}
+}
